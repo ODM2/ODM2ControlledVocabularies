a3bfd7e74f727f478cb6d93a0549e1b765cc6d72
Juan <juan.caraballo17@gmail.com>
2020-12-16 10:21:24 -0400

    Setting up wsgi.py to use `linux_server` settings file.

--- src/odm2cvs/wsgi.py
+++ src/odm2cvs/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoProject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'odm2cvs.settings.linux_server')
 
 application = get_wsgi_application()
