08bb1a960e83b0b40e5250a0119e5c4413e225c2
Juan <juan.caraballo17@gmail.com>
2021-01-18 13:14:45 -0400

    Changed `reset_db.py` to work with the latest code update.

--- src/cvservices/management/commands/reset_db.py
+++ src/cvservices/management/commands/reset_db.py
@@ -1,115 +1,12 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 
-from cvservices.models import AggregationStatistic, ApplicableResourceType, BeneficialUseCategory, CoordinateMethod, \
-    CustomerType, \
-    CropType, DataQualityValue, EPSGCode, GNISFeatureName, IrrigationMethod, \
-    LegalStatus, MethodType, NAICSCode, \
-    NHDNetworkStatus, NHDProduct, PowerType, RegulatoryStatus, RegulatoryOverlayType, ReportingUnitType, ReportYear, \
-    ReportYearType, SDWISIdentifier, States, SiteType, Units, \
-    USGSCategory, Variable, VariableSpecific, WaterAllocationBasis, WaterQualityIndicator, \
-    WaterAllocationType, WaterSourceType
+from odm2cvs.controlled_vocabularies import vocabularies
 
 
 class Command(BaseCommand):
     help = 'Deletes every object in the database'
 
-    # def add_arguments(self, parser):
-    #    pass
-
     def handle(self, *args, **options):
-
-        for object in AggregationStatistic.objects.all():
-            object.delete()
-
-        for object in ApplicableResourceType.objects.all():
-            object.delete()
-
-        for object in BeneficialUseCategory.objects.all():
-            object.delete()
-
-        for object in CoordinateMethod.objects.all():
-            object.delete()
-
-        for object in CustomerType.objects.all():
-            object.delete()
-
-        for object in CropType.objects.all():
-            object.delete()
-
-        for object in DataQualityValue.objects.all():
-            object.delete()
-
-        for object in EPSGCode.objects.all():
-            object.delete()
-
-        for object in GNISFeatureName.objects.all():
-            object.delete()
-
-        for object in IrrigationMethod.objects.all():
-            object.delete()
-
-        for object in LegalStatus.objects.all():
-            object.delete()
-
-        for object in MethodType.objects.all():
-            object.delete()
-
-        for object in NAICSCode.objects.all():
-            object.delete()
-
-        for object in NHDNetworkStatus.objects.all():
-            object.delete()
-
-        for object in NHDProduct.objects.all():
-            object.delete()
-
-        for object in PowerType.objects.all():
-            object.delete()
-
-        for object in RegulatoryOverlayType.objects.all():
-            object.delete()
-
-        for object in RegulatoryStatus.objects.all():
-            object.delete()
-
-        for object in ReportingUnitType.objects.all():
-            object.delete()
-
-        for object in ReportYear.objects.all():
-            object.delete()
-
-        for object in ReportYearType.objects.all():
-            object.delete()
-
-        for object in SDWISIdentifier.objects.all():
-            object.delete()
-
-        for object in SiteType.objects.all():
-            object.delete()
-
-        for object in States.objects.all():
-            object.delete()
-
-        for object in Units.objects.all():
-            object.delete()
-
-        for object in USGSCategory.objects.all():
-            object.delete()
-
-        for object in Variable.objects.all():
-            object.delete()
-
-        for object in VariableSpecific.objects.all():
-            object.delete()
-
-        for object in WaterAllocationBasis.objects.all():
-            object.delete()
-
-        for object in WaterQualityIndicator.objects.all():
-            object.delete()
-
-        for object in WaterAllocationType.objects.all():
-            object.delete()
-
-        for object in WaterSourceType.objects.all():
-            object.delete()
+        for vocabulary_code, vocabulary in vocabularies.items():
+            vocabulary.get('model').objects.all().delete()
+            vocabulary.get('request').get('model').objects.all().delete()
