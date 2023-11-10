122af566f355fd90e543e387cdee0e480a80e991
pkdash <pkdash_reena@hotmail.com>
2023-11-10 16:34:07 -0500

    [#82] making changes to wsgi.py for EB to find the settings module

--- src/odm2cvs/odm2cvs/wsgi.py
+++ src/odm2cvs/odm2cvs/wsgi.py
@@ -10,9 +10,12 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(BASE_DIR, os.pardir))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'odm2cvs.settings')
 
 application = get_wsgi_application()
